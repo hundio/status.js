78064469e7c92c1f9978ab857c879ff623bc7471
Jonathan la Cour <jon@lacour.me>
2018-05-18 15:19:18 -0600

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -11,45 +11,47 @@ gulp.task("clean", function() {
   return del(["build", "dist", "status.min.js"]);
 });
 
-gulp.task("coffeescript", ["clean"], function() {
+gulp.task("coffeescript", function() {
   return gulp.src("./src/*.coffee")
     .pipe(coffee({ bare: true }).on("error", gutil.log))
     .pipe(gulp.dest("./build/"));
 });
 
-gulp.task("sass", ["clean"], function() {
+gulp.task("sass", function() {
   return gulp.src("./src/styles/status.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("compile", ["clean", "coffeescript"], function() {
+gulp.task("compile", function() {
   return gulp.src("build/status.js")
     .pipe(closureCompiler({
       fileName: "status.min.js",
       compilerFlags: {
         closure_entry_point: "status.main",
-        compilation_level: "ADVANCED_OPTIMIZATIONS",
+        compilation_level: "SIMPLE_OPTIMIZATIONS",
         extra_annotation_name: "Status",
+        language_in: "ECMASCRIPT6",
+        language_out: "ECMASCRIPT5",
         only_closure_dependencies: true
       }
     }))
     .pipe(gulp.dest("./build/"));
 });
 
-gulp.task("vendor", ["clean", "coffeescript"], function() {
+gulp.task("vendor", function() {
   return gulp.src("./vendor/*.js")
     .pipe(strip())
     .pipe(gulp.dest("./build/"));
 });
 
-gulp.task("dist", ["clean", "coffeescript", "compile", "vendor"], function() {
+gulp.task("dist", function() {
   return gulp.src("./build/*.min.js")
     .pipe(concat("status.js"))
     .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("default", ["clean", "coffeescript", "sass", "compile", "vendor", "dist"]);
+gulp.task("default", gulp.series("clean", "coffeescript", "sass", "compile", "vendor", "dist"));
 
 gulp.task("watch", function() {
   var watcher = gulp.watch("./src/**/*", ["default"]);
